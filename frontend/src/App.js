520f966b5e075376b34f10ab7cbbc80161610311
Marcin Wasilewski <me@marcinwasilewski.net>
2017-12-15 23:22:54 +0100

    some more initial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Drawer from "material-ui/Drawer";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RaisedButton label="Material UI" />
-      </div>
-      </MuiThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <Drawer
+                        type="permanent"
+                        anchor="left">
+                        <div>test</div>
+                    </Drawer>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
