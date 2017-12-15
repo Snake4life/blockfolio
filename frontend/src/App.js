cf609f13cdbb071ccc80a9f1f898615732b2a523
Marcin Wasilewski <me@marcinwasilewski.net>
2017-12-16 00:24:55 +0100

    modified persistent drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import Drawer from "material-ui/Drawer";
-import IconButton from "material-ui/IconButton";
-import AppBar from 'material-ui/AppBar';
-import MenuIcon from 'material-ui-icons/Menu';
-import Typography from 'material-ui/Typography';
-import Toolbar from 'material-ui/Toolbar';
+import PersistentDrawer from "./PersistentDrawer";
 import "./App.css";
 
 class App extends Component {
@@ -23,26 +18,7 @@ class App extends Component {
         return (
             <MuiThemeProvider>
                 <div className="App">
-                    <AppBar>
-                    <Toolbar disableGutters={!this.state.drawerOpen}>
-                      <IconButton
-                        color="contrast"
-                        aria-label="open drawer"
-                        onClick={this.handleDrawerOpen}
-                      >
-                        <MenuIcon />
-                      </IconButton>
-                      <Typography type="title" color="inherit" noWrap>
-                        Persistent drawer
-                      </Typography>
-                    </Toolbar>
-                  </AppBar>
-                    <Drawer
-                        type="persistent"
-                        anchor="left"
-                        open={this.state.drawerOpen}>
-                        <div>Drawer</div>
-                    </Drawer>
+                    <PersistentDrawer/>
                 </div>
             </MuiThemeProvider>
         );
