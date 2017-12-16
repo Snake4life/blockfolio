45672a2d5c589a30dcd129ca3963ba480fae102a
Marcin Wasilewski <me@marcinwasilewski.net>
2017-12-16 20:22:34 +0100

    added a responsive drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import PersistentDrawer from "./PersistentDrawer";
+import ResponsiveDrawer from "./ResponsiveDrawer";
 import "./App.css";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            drawerOpen:false
+           
         }
-        this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
     }
-    handleDrawerOpen = () => {
-        this.setState({ drawerOpen: true });
-    };
     render() {
         return (
             <MuiThemeProvider>
                 <div className="App">
-                    <PersistentDrawer/>
+                    <ResponsiveDrawer/>
                 </div>
             </MuiThemeProvider>
         );
