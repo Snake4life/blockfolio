c27d94744c5f041e17177b09b5a4f8b518a1c4b3
Marcin Wasilewski <me@marcinwasilewski.net>
2018-01-10 01:52:07 +0100

    timezone 0

--- backend/bin/fetchHistoricalPrices.js
+++ backend/bin/fetchHistoricalPrices.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     password: config.db.password,
     database: config.db.database,
     port: 3306,
-    dateStrings : true
+    timezone: '0'
 });
 
 Date.prototype.addDays = function(days) {
